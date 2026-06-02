f6f2a1bc0dc2dec688dd18bb3f3796a1d1d7c851
Super User <root@ip-172-31-85-75.ec2.internal>
2026-06-02 15:51:26 +0000

    files addedd successfully

--- fact.c
+++ fact.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-
-int main()
-{
+ factorial() {
     int n, i;
-    long long fact = 1;
-
-    printf("Enter a number: ");
+    unsigned long long fact = 1;
+    printf("Enter an integer: ");
     scanf("%d", &n);
 
-    for(i = 1; i <= n; i++)
-    {
-        fact = fact * i;
+    // shows error if the user enters a negative integer
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
     }
 
-    printf("Factorial of %d = %lld\n", n, fact);
-
-    return 0;
+   // return 0;
 }
+
